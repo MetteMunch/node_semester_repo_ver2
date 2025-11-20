037207b055fe931ec6d8fd468696d04a55a65b34
MetteMunch <meha0005@stud.kea.dk>
2025-11-20 11:27:31 +0100

    arbejdedde på 11 - færddiggør

---
+++ EK_DAT_Node.js_2025_Autumn/11_sql_Fitness/app.js
@@ -0,0 +1,21 @@
+import express  from "express";
+
+const app = express();
+
+
+
+app.use(express.json);
+
+import exercisesRouter from './routers/exercisesRouters.js';
+app.use(exercisesRouter);
+import usersRouter from './routers/usersRouter.js';
+app.use(usersRouter);
+
+
+
+
+const PORT = Number(process.env.PORT) || 8080;
+
+app.listen(PORT, () => {
+    console.log("Server is running on ", PORT);
+})
---
+++ EK_DAT_Node.js_2025_Autumn/11_sql_Fitness/database/connection.js
@@ -0,0 +1,11 @@
+import sqlite3 from 'sqlite3'
+import { open } from 'sqlite'
+
+const connection = await open({
+    filename: "fitnessdb.db",
+    driver: sqlite3.Database
+});
+
+
+
+export default connection;
